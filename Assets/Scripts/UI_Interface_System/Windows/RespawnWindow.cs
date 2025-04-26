7fc9c342328927bbd0b377bdcd28334137b9b2ee
GriMor <GriMorProgram@yandex.ru>
2025-04-27 02:16:54 +0300

    Update level + fix bug with respawn + add hide to respawn point + spaw raycast ground detect system to sphere overlap

--- Assets/Scripts/UI_Interface_System/Windows/RespawnWindow.cs
+++ Assets/Scripts/UI_Interface_System/Windows/RespawnWindow.cs
@@ -1,4 +1,5 @@
 using PlayerControllers;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,19 +9,31 @@ public class RespawnWindow : AbstractWindow
     [SerializeField] private Button _respawnBtn;
     [SerializeField] private Button _exitToMainMenuBtn;
 
+    bool _respawnInit = false;
+
     public override void Init(SupportClasses.WindowName parentWin = SupportClasses.WindowName.None)
     {
         base.Init(parentWin);
 
         _respawnBtn.onClick.AddListener(() =>
         {
-            CloseWindow();
-            FindAnyObjectByType<PlayerController>().PlayerRespawn();
+            if (!_respawnInit)
+                InitRespawnPlayer();
         });
 
         _exitToMainMenuBtn.onClick.AddListener(() =>
         {
             SceneManager.LoadScene(0);
         });
+    }
+
+    async void InitRespawnPlayer()
+    {
+        _respawnInit = true;
+
+        await Task.Delay(500);
+        FindAnyObjectByType<PlayerController>().PlayerRespawn();
+        CloseWindow();
+        _respawnInit = false;
     }
 }
